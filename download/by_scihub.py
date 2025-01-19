a467ec4437a14fef4549c501358f8f0af38ebadc
weidong <2405320456@qq.com>
2025-01-19 22:38:25 +0800

    scihub的下载debug

--- download/by_scihub.py
+++ download/by_scihub.py
@@ -9,10 +9,12 @@ def download_pdf(paper, paper_type, save_dir, logger):
     name = make_uname_for_file()
     out = os.path.join(save_dir, name)
     scihub_download(paper, paper_type=paper_type, out=out)
-    if os.path.exists(out):
-        logger.debug(f'scidownl 下载完成 {paper}')
+    if os.path.exists(out + '.pdf'):  # debug 会自己加后缀
+        os.rename(out + '.pdf', out)  # 重命名文件
+        logger.debug(f'scidownl 下载完成 {paper}, {out}')
         return name
     else:
+        logger.error(f'scidownl下载失败 {paper}, {out}')
         raise DownloadFailed(f'scidownl下载失败')
 
 
